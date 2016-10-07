860a279b3b207ac13338a618b12ee0bd8767a7fe
qiwsir <qiwsir@gmail.com>
2016-10-07 22:01:51 +0800

    title list

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from .models import BlogArticles
 
-# Create your views here.
+def blog_title(request):
+    blogs = BlogArticles.objects.all()
+    return render(request, "blog/titles.html", {"blogs":blogs})
