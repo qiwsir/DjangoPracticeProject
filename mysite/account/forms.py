8dfbc45809d0adf24e6af66f65b4b8aa377003eb
qiwsir <qiwsir@gmail.com>
2016-10-22 17:15:52 +0800

    new forms

--- mysite/account/forms.py
+++ mysite/account/forms.py
@@ -3,24 +3,22 @@ from django.contrib.auth.models import User
 from .models import UserProfile
 
 class UserForm(forms.ModelForm):
-    pass
-#    password = forms.CharField(label="Password", widget=forms.PasswordInput)
-#    password2 = forms.CharField(label="Confirm Password", widget=forms.PasswordInput)
+    password = forms.CharField(label="Password", widget=forms.PasswordInput)
+    password2 = forms.CharField(label="Confirm Password", widget=forms.PasswordInput)
 
-#    class Meta:
-#        model = User
-#        fields = ('username', 'email')
+    class Meta:
+        model = User
+        fields = ('username', 'email')
 
-#    def clean_password2(self):
-#        cd = self.cleaned_data
-#        if cd['password'] != cd['password2']:
-#            raise forms.ValidationError("Passwords do not math.")
-#        return cd['password2']
+    def clean_password2(self):
+        cd = self.cleaned_data
+        if cd['password'] != cd['password2']:
+            raise forms.ValidationError("Passwords do not math.")
+        return cd['password2']
 
 
 class UserProfileForm(forms.ModelForm):
-    pass
-#    class Meta:
-#        model = UserProfile
+    class Meta:
+        model = UserProfile
 
 
