0f6a34edda06cabd3caac4fc73070d43651ea6d4
qiwsir <qiwsir@gmail.com>
2016-11-01 18:36:48 +0800

    simple register form class

--- mysite/account/forms.py
+++ mysite/account/forms.py
@@ -1,5 +1,20 @@
 from django import forms
+from django.contrib.auth.models import User
 
 class LoginForm(forms.Form):
     username = forms.CharField()
     password = forms.CharField(widget=forms.PasswordInput)
+
+class RegistrationForm(forms.ModelForm):
+    password = forms.CharField(label="Password", widget=forms.PasswordInput)
+    password2 = forms.CharField(label="Confirm Pssword", widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ("username",)
+
+    def clean_password2(self):
+        cd = self.cleaned_data
+        if cd['password'] != cd['password2']:
+            raise forms.ValidationError("passwords do not match.")
+        return cd['password']
