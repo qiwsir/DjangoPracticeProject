df6a44f843c35ccacb4ea75dff5065e196027cd5
qiwsir <qiwsir@gmail.com>
2016-11-03 23:11:44 +0800

    profile user register

--- mysite/account/forms.py
+++ mysite/account/forms.py
@@ -1,5 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
+from .models import UserProfile
+
 
 class LoginForm(forms.Form):
     username = forms.CharField()
@@ -18,3 +20,8 @@ class RegistrationForm(forms.ModelForm):
         if cd['password'] != cd['password2']:
             raise forms.ValidationError("passwords do not match.")
         return cd['password2']
+
+class UserProfileForm(forms.ModelForm):
+    class Meta:
+        model = UserProfile
+        fields = "__all__"
--- mysite/account/models.py
+++ mysite/account/models.py
@@ -1,1 +1,10 @@
+from django.db import models
+from django.contrib.auth.models import User
 
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, unique=True)
+    birth = models.DateField(blank=True, null=True)
+    phone = models.CharField(max_length=20, null=True)
+
+    def __str__(self):
+        return 'user {}'.format(self.user.username)
