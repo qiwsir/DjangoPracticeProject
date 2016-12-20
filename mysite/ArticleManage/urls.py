4e0dcfe73d5f502bf3f9b708374b156369a56352
qiwsir <qiwsir@gmail.com>
2016-12-20 22:14:55 +0800

    article detail

--- mysite/ArticleManage/urls.py
+++ mysite/ArticleManage/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     url(r'^del-column/$', views.del_article_column, name="del_article_column"),
     url(r'^article-post/$', views.article_post, name="article_post"),
     url(r'^article-list/$', views.article_list, name="article_list"),
+    url(r'^article-detail/(?P<id>\d+)/(?P<slug>[-\w]+)/$', views.article_detail, name="article_detail"),
 ]
