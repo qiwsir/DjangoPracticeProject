3e723c50c2634149b9edc3694082bdf3c912da1b
qiwsir <qiwsir@gmail.com>
2017-01-09 21:30:37 +0800

    create comment model

--- mysite/article/models.py
+++ mysite/article/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.core.urlresolvers import reverse
 
-# Create your models here.
+from ArticleManage.models import ArticlePost
+
+class Comment(models.Model):
+	article = models.ForeignKey(ArticlePost, related_name="comments")
+	commentator = models.ForeignKey(User, related_name="commentator")
+	body = models.TextField()
+	created = models.DateTimeField(auto_now_add=True)
+
+	class Meta:
+		ordering = ('-created',)
+
+	def __str__(self):
+		return "Comment by {0} on {1}".format(self.commentator.username, self.article)
