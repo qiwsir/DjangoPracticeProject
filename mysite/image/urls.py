b381a1d6716591419f34e24c2264af7332cef00a
qiwsir <qiwsir@gmail.com>
2017-01-21 19:09:13 +0800

    add image

---
+++ mysite/image/urls.py
@@ -0,0 +1,7 @@
+from django.conf.urls import url
+from . import views
+
+urlpatterns = [
+    url(r'^list-images/$', views.list_images, name="list_images"),
+    url(r'^upload-image/$', views.upload_image, name='upload_image'),
+]
--- mysite/image/views.py
+++ mysite/image/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.http import JsonResponse
+from django.views.decorators.http import require_POST
+from django.views.decorators.csrf import csrf_exempt
 
-# Create your views here.
+from .forms import ImageForm
+from .models import Image
+
+@login_required(login_url='/account/login/')
+@require_POST
+@csrf_exempt
+def upload_image(request):
+    form = ImageForm(data=request.POST)
+    if form.is_valid():
+        try:
+            new_item = form.save(commit=False)
+            new_item.user = request.user
+            new_item.save()
+            return JsonResponse({'status':"1"})
+        except:
+            return JsonResponse({'status':"0"})
+
+@login_required(login_url='/account/login/')
+def list_images(request):
+    images = Image.objects.filter(user=request.user)
+    return render(request, 'image/list_images.html', {"images": images})
