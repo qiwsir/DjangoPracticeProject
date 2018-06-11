62e3454066e20cb661a6e52761eee0d69c25afc6
Chee Wei <qiwsir@Chees-MBP.lan>
2018-06-11 18:44:42 +0800

    display the title of blog

--- mysite2/blog/views.py
+++ mysite2/blog/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from .models import BlogArticles
 
-# Create your views here.
+def blog_title(request):    #②
+    blogs = BlogArticles.objects.all()    #③
+    return render(request, "blog/titles.html", {"blogs":blogs})    #④
