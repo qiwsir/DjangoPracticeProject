422f43896691809f6487aa22e86dd9b832c77160
Chee Wei <qiwsir@Chees-MBP.lan>
2018-06-11 16:58:04 +0800

    migrate db for blog application

--- mysite2/blog/models.py
+++ mysite2/blog/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+class BlogArticles(models.Model):
+    title = models.CharField(max_length=300)    #①
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="blog_posts")    #②
+    body = models.TextField()
+    publish = models.DateTimeField(default=timezone.now)
+
+    class Meta:    #③
+        ordering = ("-publish",)
+
+    def __str__(self):
+        return self.title
