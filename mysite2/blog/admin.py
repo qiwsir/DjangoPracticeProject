e8f872a7bfa43cfc17454019b9e92e222156ffbe
Chee Wei <qiwsir@Chees-MBP.lan>
2018-06-11 17:34:49 +0800

    publish articles and list articles

--- mysite2/blog/admin.py
+++ mysite2/blog/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import BlogArticles    #①
 
-# Register your models here.
+#admin.site.register(BlogArticles)    #②
+
+class BlogArticlesAdmin(admin.ModelAdmin):
+    list_display = ("title", "author", "publish")
+    list_filter = ("publish", "author")
+    search_fields = ('title', "body")
+    raw_id_fields = ("author",)
+    date_hierarchy = "publish"
+    ordering = ['-publish', 'author']
+
+admin.site.register(BlogArticles, BlogArticlesAdmin)
