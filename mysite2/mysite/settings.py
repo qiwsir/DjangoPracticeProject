da5a888fa6aa44aed5afedf02363a907355bbea5
Chee Wei <qiwsir@Chees-MBP.lan>
2018-06-11 14:33:32 +0800

    create a application: blog

--- mysite2/mysite/settings.py
+++ mysite2/mysite/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'blog',
 ]
 
 MIDDLEWARE = [
@@ -105,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Shanghai'
 
 USE_I18N = True
 
