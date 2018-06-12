36687e53976f86b91ac5060ca94c5c020992abff
Chee Wei <qiwsir@Chees-MBP.lan>
2018-06-12 17:55:00 +0800

    append admin of users

--- mysite2/account/admin.py
+++ mysite2/account/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import UserProfile
 
-# Register your models here.
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'birth', 'phone')
+    list_filter = ("phone",)
+
+admin.site.register(UserProfile, UserProfileAdmin)
