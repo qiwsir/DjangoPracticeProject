be7993abda3ef85c8c6ef99f5b1e004c00fd73c5
Chee Wei <qiwsir@Chees-MBP.lan>
2018-06-12 14:40:07 +0800

    define static and templates

--- mysite2/blog/urls.py
+++ mysite2/blog/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
 
+app_name = "blog"
+
 urlpatterns = [
-    path('', views.blog_title),
-    path('<int:article_id>/', views.blog_article),
+    path('', views.blog_title, name='blog_title'),
+    path('<int:article_id>/', views.blog_article, name='blog_article'),
 ]
--- mysite2/mysite/urls.py
+++ mysite2/mysite/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('blog/', include('blog.urls')),
+    #path('blog/', include(('blog.urls', 'blog'), namespace='blog')),
+    path('blog/', include('blog.urls', namespace='blog')),
 ]
