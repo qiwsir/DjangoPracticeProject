6ca876baab1b0deea6447ca6a5d737a38bec7cc2
Chee Wei <qiwsir@Chees-MBP.lan>
2018-06-12 19:09:47 +0800

    change password

--- mysite2/account/urls.py
+++ mysite2/account/urls.py
@@ -11,4 +11,6 @@ urlpatterns = [
     path('new-login/', auth_views.login, {'template_name': 'account/login.html'}, name='user_login'),
     path('logout/', auth_views.logout, {'template_name': 'account/logout.html'}, name='user_logout'),
     path('register/', views.register, name='user_register'),
+    path('password-change/', auth_views.password_change, {"post_change_redirect":"/account/password-change-done", 'template_name': 'account/password_change_form.html'}, name='password_change'),
+    path('password-change-done/', auth_views.password_change_done, {'template_name': 'account/password_change_done.html'}, name='password_change_done'),
 ]
