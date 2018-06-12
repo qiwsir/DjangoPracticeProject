b6fa86375b58b419e436c14ed3d2dafe9f53d6d7
Chee Wei <qiwsir@Chees-MBP.lan>
2018-06-12 16:56:02 +0800

    builtin login and logout

--- mysite2/account/urls.py
+++ mysite2/account/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
 from . import views
 
+from django.contrib.auth import views as auth_views
+
 app_name = "account"
 
 urlpatterns = [
-    path('login/', views.user_login, name='user_login'),
+    #path('login/', views.user_login, name='user_login'),
+    path('login/', auth_views.login, name='user_login'),
+    path('new-login/', auth_views.login, {'template_name': 'account/login.html'}, name='user_login'),
+    path('logout/', auth_views.logout, {'template_name': 'account/logout.html'}, name='user_logout'),
 ]
--- mysite2/mysite/settings.py
+++ mysite2/mysite/settings.py
@@ -123,3 +123,5 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
 )
+
+LOGIN_REDIRECT_URL = '/blog/'
