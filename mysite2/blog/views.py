0af49d8a1833ef77e16fce38b6f6f2b26227ef77
Chee Wei <qiwsir@Chees-MBP.lan>
2018-06-12 11:20:42 +0800

    get object or 404

--- mysite2/blog/views.py
+++ mysite2/blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import BlogArticles
 
 def blog_title(request):    #②
@@ -6,6 +6,7 @@ def blog_title(request):    #②
     return render(request, "blog/titles.html", {"blogs":blogs})    #④
 
 def blog_article(request, article_id):    #①
-    article = BlogArticles.objects.get(id=article_id)    #②
+    #article = BlogArticles.objects.get(id=article_id)    #②
+    article = get_object_or_404(BlogArticles, id=article_id)
     pub = article.publish
     return render(request, "blog/content.html", {"article":article, "publish":pub })
