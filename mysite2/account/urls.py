7d0a97e58df46c76120f337338120f5a4545d4eb
Chee Wei <qiwsir@Chees-MBP.lan>
2018-06-12 19:51:19 +0800

    password reset templates

--- mysite2/account/urls.py
+++ mysite2/account/urls.py
@@ -13,4 +13,9 @@ urlpatterns = [
     path('register/', views.register, name='user_register'),
     path('password-change/', auth_views.password_change, {"post_change_redirect":"/account/password-change-done", 'template_name': 'account/password_change_form.html'}, name='password_change'),
     path('password-change-done/', auth_views.password_change_done, {'template_name': 'account/password_change_done.html'}, name='password_change_done'),
+    path('password-reset/', auth_views.password_reset, {"template_name":"account/password_reset_form.html", "email_template_name":"account/password_reset_email.html", "subject_template_name":"account/password_reset_subject.txt",  "post_reset_redirect":"/account/password-reset-done"}, name="password_reset"),
+    path('password-reset-done/', auth_views.password_reset_done, {"template_name":"account/password_reset_done.html"}, name="password_reset_done"),
+    path('password-reset-confirm/<uidb64>/<token>/', auth_views.password_reset_confirm, {"template_name":"account/password_reset_confirm.html", "post_reset_redirect":"/account/password-reset-complete"}, name="password_reset_confirm"),
+    path('password-reset-complete/', auth_views.password_reset_complete, {"template_name":"account/password_reset_complete.html"}, name="password_reset_complete"),
+
 ]
