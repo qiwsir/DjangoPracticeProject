0e53d7a943c00f51bd0b4c2a3f9a97d3eb8c60eb
Chee Wei <qiwsir@promote.cache-dns.local>
2018-06-16 21:12:20 +0800

    filter tag

--- mysite2/article/templatetags/article_tags.py
+++ mysite2/article/templatetags/article_tags.py
@@ -3,6 +3,11 @@ from django import template    #①
 register = template.Library()    #②
 
 from article.models import ArticlePost    #③
+from django.db.models import Count
+
+from django.utils.safestring import mark_safe
+import markdown
+
 
 @register.simple_tag    #④
 def total_articles():
@@ -16,3 +21,13 @@ def author_total_articles(user):    #⑧
 def latest_articles(n=5):    #②
 	latest_articles = ArticlePost.objects.order_by("-created")[:n]    #③
 	return {"latest_articles": latest_articles}    #④
+
+
+@register.simple_tag    #①
+def most_commented_articles(n=3):    #②
+	return ArticlePost.objects.annotate(total_comments=Count('comments')).order_by("-total_comments")[:n]    #③
+
+
+@register.filter(name='markdown')    #①
+def markdown_filter(text):    #②
+	return mark_safe(markdown.markdown(text))    #③
